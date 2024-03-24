0b6b2bd00b1902706664dbf9bf6a681c5414228e
chris.tmars <chris.tmars@kakaocorp.com>
2024-03-24 17:29:05 +0900

    feat : ui 기능 구현 - InputView : 입력을 받기위한 view - OutputView : 출력에 관한 view

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,11 @@
+package view;
+
+public class InputView {
+	public static void displayInputMessage() {
+		System.out.print("숫자를 입력해주세요 : ");
+	}
+
+	public static void displayRestartMessage() {
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+	}
+}
